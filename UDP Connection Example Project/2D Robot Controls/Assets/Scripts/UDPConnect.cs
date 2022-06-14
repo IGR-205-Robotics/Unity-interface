486e8e847a3d080e545fd824d3b7de79f9d628f2
Bex <rebeccaruva@newschool.edu>
2022-06-14 17:38:28 +0200

    update interface to test basic commands

--- UDP Connection Example Project/2D Robot Controls/Assets/Scripts/UDPConnect.cs
+++ UDP Connection Example Project/2D Robot Controls/Assets/Scripts/UDPConnect.cs
@@ -8,20 +8,20 @@ using System;
 
 public class UDPConnect : MonoBehaviour
 {
+    UdpClient client;
+    byte[] bytesToSend;
+    IPEndPoint remoteEndPoint;
+    bool update = true;
+
     void Start()
     {
-        UDPTest();
-    }
-    
-    void UDPTest()
-    {
-        UdpClient client = new UdpClient(5600);
+        client = new UdpClient(5600);
         try
         {
             client.Connect("127.0.0.1", 5500);
-            byte[] sendBytes = Encoding.ASCII.GetBytes("Hello, from the client");
-            client.Send(sendBytes, sendBytes.Length);
-            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 5500);
+            bytesToSend = Encoding.ASCII.GetBytes("TEST");
+            client.Send(bytesToSend, bytesToSend.Length);
+            remoteEndPoint = new IPEndPoint(IPAddress.Any, 5500);
             byte[] receiveBytes = client.Receive(ref remoteEndPoint);
             string receivedString = Encoding.ASCII.GetString(receiveBytes);
             print("Message received from the server \n " + receivedString);
@@ -30,5 +30,43 @@ public class UDPConnect : MonoBehaviour
         {
             print("Exception thrown " + e.Message);
         }
+    }
+
+    void Update() {
+        if(update) {
+            //send the new bytes to server
+            try{
+                client.Send(bytesToSend, bytesToSend.Length);
+                remoteEndPoint = new IPEndPoint(IPAddress.Any, 5500);
+                byte[] receiveBytes = client.Receive(ref remoteEndPoint);
+                string receivedString = Encoding.ASCII.GetString(receiveBytes);
+                print("Message received from the server \n " + receivedString);
+            }catch(Exception e) {
+                print("Exception thrown " + e.Message);
+            }
+            update = false;
+        }
+    }
+
+    public void SendDirection(int dir) {
+        //int : dir
+        /**
+        0: top
+        1: left
+        **/
+
+        switch(dir) {
+            case 0:
+                bytesToSend = Encoding.ASCII.GetBytes("top");
+                break;
+            case 1:
+                bytesToSend = Encoding.ASCII.GetBytes("left");
+                break;
+            case 2:
+                bytesToSend = Encoding.ASCII.GetBytes("TEST");
+                break;
+        }
+
+        update = true;
     }
 }
